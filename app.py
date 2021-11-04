488e1d85f80e2179dd7db5cba499409c957e633e
Johnny <johnny85215@gmail.com>
2021-11-05 00:49:04 +0800

    remove price

--- app.py
+++ app.py
@@ -5,10 +5,9 @@ from main import Trading_bot
 eel.init('web')
 
 @eel.expose
-def run_trade_bot(api_key, api_secret_key, amount, price):
+def run_trade_bot(api_key, api_secret_key, amount):
     amount = int(amount) if amount.isdigit() else 500000
-    price = float(price) if re.match(r'^-?\d+(?:\.\d+)$', price) is not None else 0.0001
-    run=Trading_bot(api_key, api_secret_key, amount=amount, price=price)
+    run=Trading_bot(api_key, api_secret_key, amount=amount)
     run()
 
 eel.start('main.html', size=(1200, 800))
